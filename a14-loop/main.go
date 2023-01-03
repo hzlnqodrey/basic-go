89c6a2847afd2405d5497608c9ed2a4547eef063
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-03 23:57:48 +0700

    all the basic for loop variants

--- a14-loop/main.go
+++ a14-loop/main.go
@@ -3,7 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	// basic for
 	for i := 0; i < 5; i++ {
-		fmt.Printf("Angka %d\n", i+1)
+		fmt.Printf("Angka 1st %d\n", i+1)
+	}
+
+	// for, only conditions without args
+	var i = 0
+
+	for i < 5 {
+		fmt.Printf("Angka 2nd %d\n", i+1)
+		i++
+	} 
+
+	// for, no args no conds
+	var i2 = 0
+
+	for {
+
+		fmt.Printf("Angka 3rd %d\n", i2+1)
+
+		i2++ 
+		
+		if i2 >= 5 {
+			break
+		}
+		
 	}
 }
