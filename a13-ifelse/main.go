49120d6945d5c14648a7e758039cc8d83ac17d30
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-03 19:46:45 +0700

    ifelse

---
+++ a13-ifelse/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// if else
+	var point = -1
+
+	if point == 8 {
+		fmt.Println("lulus dengan nilai sempurna")
+	} else if point > 5 {
+		fmt.Println("lulus")
+	} else if point == 4 {
+		fmt.Println("hampir lulus")
+	} else {
+		fmt.Printf("tidak lulus, nilai anda: %d\n", point)
+	}
+
+	// variable temporary
+
+	// switch case
+}
