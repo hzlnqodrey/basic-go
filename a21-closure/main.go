edba07e37c655defe726bfd2b9f798e75eafddc0
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-21 10:57:59 +0700

    closure IIFE - Immediately Invoked Function Expression

--- a21-closure/main.go
+++ a21-closure/main.go
@@ -4,26 +4,52 @@ import "fmt"
 
 func main() {
 
-	var getMinMax = func(n []int) (int, int) {
-		var min, max int
+	// 1 - Closure
+	// var getMinMax = func(n []int) (int, int) {
+	// 	var min, max int
 
-		for i, e := range n {
-			switch {
-			case i == 0:
-				max, min = e, e
-			case e > max:
-				max = e
-			case e < min:
-				min = e
+	// 	for i, e := range n {
+	// 		switch {
+	// 		case i == 0:
+	// 			max, min = e, e
+	// 		case e > max:
+	// 			max = e
+	// 		case e < min:
+	// 			min = e
+	// 		}
+	// 	}
+
+	// 	return min, max
+	// }
+
+	// var numbers = []int{2, 9, 8, 10, 4, 2, 3, 7}
+
+	// var min, max = getMinMax(numbers)
+
+	// fmt.Printf("data: %v\nmin : %v\nmax: %v\n", numbers, min, max)
+
+	// 2 - Clousre IIFE
+	
+	var numbers  = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+
+	// filtered number that below 3, delete them
+	var filteredNumbers = func(min int) []int {
+
+		var r []int
+
+		for _, e := range numbers {
+			if e < min {
+				continue
 			}
+				
+			r = append(r, e)
 		}
-		
-		return min, max
-	}
 
-	var numbers = []int{2, 9, 8, 10, 4, 2, 3, 7}
+		return r
 
-	var min, max = getMinMax(numbers)
+	}(3)
 
-	fmt.Printf("data: %v\nmin : %v\nmax: %v\n", numbers, min, max)
+	fmt.Printf("original number: %v\n", numbers)
+	fmt.Printf("filtered number: %v\n", filteredNumbers)
+
 }
