fce787f2bc33b9dc57d6c1f43c6e20dfa1735048
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-21 16:18:32 +0700

    another struct declaration

--- a24-struct/main.go
+++ a24-struct/main.go
@@ -16,6 +16,13 @@ func main() {
 	s1.name = "Hazlan Muhammad Qodri"
 	s1.grade = 90
 
+	var s2 = student{"ethan", 20}
+
+	var s3 = student{name: "jason"}
+
 	fmt.Println("name : ", s1.name)
 	fmt.Println("grade : ", s1.grade)
+
+	fmt.Println("name : ", s2.name)
+	fmt.Println("name : ", s3.name)
 }
