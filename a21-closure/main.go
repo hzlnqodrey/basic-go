fc3b6f471e010ddeb7b167fa7dc8ee13b2c21157
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-21 10:46:16 +0700

    basic closure

---
+++ a21-closure/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+		
+		return min, max
+	}
+
+	var numbers = []int{2, 9, 8, 10, 4, 2, 3, 7}
+
+	var min, max = getMinMax(numbers)
+
+	fmt.Printf("data: %v\nmin : %v\nmax: %v\n", numbers, min, max)
+}
