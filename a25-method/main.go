8509dc4441f8d18caf3700f0ab451e246c6a4105
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-22 16:40:20 +0700

    method in go

--- a25-method/main.go
+++ a25-method/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+type student struct {
+	grade int
+	name string
+}
+
+// Method Declaration
+
+func (s student) sayHello() {
+	fmt.Println("halo ", s.name)
+}
+
+func (s student) getNickname(i int) string {
+	var Nickname = strings.Split(s.name, " ")[i - 1]
+	return Nickname
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var s1 = student{90, "john wick"}
+	s1.sayHello()
+
+	var name = s1.getNickname(2)
+	fmt.Printf("nama panggilan: %s", name)
 }
