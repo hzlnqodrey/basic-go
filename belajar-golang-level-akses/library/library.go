0223ba1d5e464d425beeb7312c4b2afaa5a2b264
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-26 14:59:45 +0700

    export import struct package in golang

--- belajar-golang-level-akses/library/library.go
+++ belajar-golang-level-akses/library/library.go
@@ -1,12 +1,6 @@
 package library
 
-import "fmt"
-
-func SayHello(name string) {
-	fmt.Println("Hello")
-	introduce(name)
-}
-
-func introduce(name string) {
-	fmt.Println("nama saya", name)
+type Student struct {
+	Name string
+	Grade int
 }
--- belajar-golang-level-akses/main.go
+++ belajar-golang-level-akses/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "belajar-golang-level-akses/library"
+import (
+	"belajar-golang-level-akses/library"
+	"fmt"
+)
 
 func main() {
-	library.SayHello("Hazlan")
+	
+	var s1 = library.Student{"Hazlan", 90}
+	
+	fmt.Println("Name: ", s1.Name)
+	fmt.Println("Grade: ", s1.Grade)
+
 }
