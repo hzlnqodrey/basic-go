8efbbecbe98cdc70257cde4747ee620fa7935df1
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-26 15:02:52 +0700

    import package dengan alias fmt jadi f

--- belajar-golang-level-akses/main.go
+++ belajar-golang-level-akses/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	. "belajar-golang-level-akses/library"
-	"fmt"
+	f "fmt"
 )
 
 func main() {
 	
 	var s1 = Student{"Hazlan", 90}
 	
-	fmt.Println("Name: ", s1.Name)
-	fmt.Println("Grade: ", s1.Grade)
+	f.Println("Name: ", s1.Name)
+	f.Println("Grade: ", s1.Grade)
 
 }
