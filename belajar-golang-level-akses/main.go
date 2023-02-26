167b48308b473d4cc66482e9363232d953183931
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-02-26 15:09:44 +0700

    how to run all the *.go in windows

--- belajar-golang-level-akses/main.go
+++ belajar-golang-level-akses/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	. "belajar-golang-level-akses/library"
-	f "fmt"
-)
-
 func main() {
-	
-	var s1 = Student{"Hazlan", 90}
-	
-	f.Println("Name: ", s1.Name)
-	f.Println("Grade: ", s1.Grade)
-
+	SayHello("Hazlan")
 }
