bb4ee429981bf25f7eb2d2b5ef0b016abeed760b
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-03-06 20:43:27 +0700

    any - type alias

--- a28-any/main.go
+++ a28-any/main.go
@@ -16,4 +16,27 @@ func main() {
 
 	secret = 12.4
 	fmt.Println(secret)
+
+	// ===
+
+	var data map[string]interface{}
+
+	data = map[string]interface{}{
+		"Nama"		: "Hazlan Muhammad Qodri",
+		"Grade"		: 90,
+		"Breakfast"	: []string{"Nasi Uduk", "Lontong"},
+	} 
+	
+	fmt.Println(data["Nama"], data["Grade"], data["Breakfast"])
+
+	// === Any [Type ALIAS]
+	var data2 map[string]any
+
+	data2 = map[string]any{
+		"Nama"		: "Hazlan Muhammad Qodri",
+		"Grade"		: 90,
+		"Breakfast"	: []string{"Nasi Uduk", "Lontong"},		
+	}
+
+	fmt.Println(data2["Nama"], data2["Grade"], data2["Breakfast"])
 }
