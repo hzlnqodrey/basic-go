b47387fcb38abc726be31b06a2d39474a0d0c873
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-03-12 17:23:36 +0700

    using channel as a parameter data type in func

--- a25-method/main.go
+++ a25-method/main.go
@@ -2,29 +2,49 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 type student struct {
-	grade int
 	name string
+	grade int
 }
 
-// Method Declaration
+// 1 -  Method Declaration
+// func (s student) sayHello() {
+// 	fmt.Println("halo ", s.name)
+// }
 
-func (s student) sayHello() {
-	fmt.Println("halo ", s.name)
+// func (s student) getNickname(i int) string {
+// 	var Nickname = strings.Split(s.name, " ")[i - 1]
+// 	return Nickname
+// }
+
+// 2 - Method Pointer
+func (s student) changeName1(name string) {
+	fmt.Println("----> on changename1, name changed to ", name)
+	s.name = name
 }
 
-func (s student) getNickname(i int) string {
-	var Nickname = strings.Split(s.name, " ")[i - 1]
-	return Nickname
+func (s *student) changeName2(name string) {
+	fmt.Println("----> on changename2, name changed to ", name)
+	s.name = name
 }
 
 func main() {
-	var s1 = student{90, "john wick"}
-	s1.sayHello()
+	// var s1 = student{90, "john wick"}
+	// s1.sayHello()
 
-	var name = s1.getNickname(2)
-	fmt.Printf("nama panggilan: %s", name)
+	// var name = s1.getNickname(2)
+	// fmt.Printf("nama panggilan: %s", name)
+
+	// 2 - Method Pointer
+	var s1 = student{"Hazlan Qodrey", 21}
+	fmt.Println("s1 before", s1.name)
+
+	s1.changeName1("Rivano ATK")
+	fmt.Println("s1 after changename1", s1.name)
+	
+	s1.changeName2("Riko A")
+	fmt.Println("s1 after changename2", s1.name)
+
 }
