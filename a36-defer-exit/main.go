eaf07548ba9a6cbf42a4855a56c89c6765b2cbb4
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-03 15:23:00 +0700

    exit

--- a36-defer-exit/main.go
+++ a36-defer-exit/main.go
@@ -2,31 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-    orderSomeFood("pizza")
-    orderSomeFood("burger")
-}
-
-func orderSomeFood(menu string) {
-    // defer fmt.Println("Terimakasih, silakan tunggu")
-    // if menu == "pizza" {
-    //     fmt.Print("Pilihan tepat!", " ")
-    //     fmt.Print("Pizza ditempat kami paling enak!", "\n")
-    //     return
-    // }
-
-    // fmt.Println("Pesanan anda:", menu)
-
-	number := 3
-
-	if number == 3 {
-		fmt.Println("halo 1")
-		func() {
-			defer fmt.Println("halo 3")
-		}()
-	}
-
-	fmt.Println("halo 2")
+	defer fmt.Println("halo dengan saya")
+	os.Exit(1)
+	fmt.Println("selamat datang")
 }
