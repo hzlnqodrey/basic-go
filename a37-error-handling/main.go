57096747c04b9d0ca46801c942a8cf1855d84ea5
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-04 10:12:43 +0700

    panic & recover in IIFE real world

--- a37-error-handling/main.go
+++ a37-error-handling/main.go
@@ -1,41 +1,47 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 )
 
-// 2 - Custom Error
-func validate(input string) (bool, error) {
-	if strings.TrimSpace(input) == "" {
-		return false, errors.New("cannot be empty")
-	}
+// // 2 - Custom Error
+// func validate(input string) (bool, error) {
+// 	if strings.TrimSpace(input) == "" {
+// 		return false, errors.New("cannot be empty")
+// 	}
 
-	return true, nil
-}
+// 	return true, nil
+// }
 
-// 3 - Recover
-func catch() {
-	if r := recover(); r != nil {
-		fmt.Println("Error Occured", r)
-	} else {
-		fmt.Println("Application Running Flawlessly")
-	}
-}
+// // 3 - Recover
+// func catch() {
+// 	if r := recover(); r != nil {
+// 		fmt.Println("Error Occured", r)
+// 	} else {
+// 		fmt.Println("Application Running Flawlessly")
+// 	}
+// }
 
 func main() {
 
-	defer catch()
+	data := []string{"superman", "aquaman", "wonder woman"}
 
-	var name string
-	fmt.Println("Type your name: ")
-	fmt.Scanln(&name)
+	for _, each := range data {
 
-	if valid, err := validate(name); valid {
-		fmt.Println("Hai", name)
-	} else {
-		panic(err.Error())
-		fmt.Println("end")
+		func() {
+
+			// recover untuk IIFE dalam perulangan
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("Panic occured on looping", each, "| message:", r)
+				} else {
+					fmt.Println("Application running perfectly")
+				}
+			}()
+
+			panic("some error happen")
+		}()
+
 	}
+
 }
