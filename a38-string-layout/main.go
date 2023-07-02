9c1c5924f59d0fc0d6b5a4fbe1310335232b8756
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-02 19:36:54 +0700

    string format done

--- a38-string-layout/main.go
+++ a38-string-layout/main.go
@@ -56,6 +56,52 @@ func main() {
 	// %p - FORMAT POINTER
 	fmt.Printf("%p\n", &data.name)
 
+	// %Q - ESCAPE FORMAT
 	fmt.Printf("%q\n", `" name \ height "`)
 	// "\" name \\ height \""
+
+	// %S - NORMAL STRING FORMAT
+	fmt.Printf("%s\n", data.hobbies)
+
+	// %t - BOOL FORMAT
+	fmt.Printf("%t\n", data.isGraduated)
+	// false
+
+	// %T - TIPE DATA FORMAT
+	fmt.Printf("%T\n", data.name)
+	// string
+	fmt.Printf("%T\n", data.height)
+	// float64
+	fmt.Printf("%T\n", data.age)
+	// int32
+	fmt.Printf("%T\n", data.isGraduated)
+	// bool
+	fmt.Printf("%T\n", data.hobbies)
+	// []string
+
+	// %V - ANY FORMAT [FREE]
+	fmt.Printf("%v\n", data)
+	// {wick 182.5 26 false [eating sleeping]}
+
+	// %+v - VARIABLE + VALUE
+	fmt.Printf("%+v\n", data)
+	// {name:wick height:182.5 age:26 isGraduated:false hobbies:[eating sleeping]}
+
+	// %#v - STRUCT FORMAT
+	fmt.Printf("%#v\n", data)
+	// main.student{name:"wick", height:182.5, age:26, isGraduated:false, hobbies:
+
+	// %x- HEXA [16]
+	fmt.Printf("%x\n", data.age)
+	// 1a
+	var d = data.name
+	fmt.Printf("%x%x%x%x\n", d[0], d[1], d[2], d[3])
+	// 7769636b
+	fmt.Printf("%x\n", d)
+	// 7769636b
+
+	// %% [yeah]
+	fmt.Printf("%%\n")
+	// %
+
 }
