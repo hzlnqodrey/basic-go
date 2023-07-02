ded7574bd7736f1f619aa180ee7514f81ee91961
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-02 19:30:42 +0700

    format pointer - %p

--- a38-string-layout/main.go
+++ a38-string-layout/main.go
@@ -27,9 +27,32 @@ func main() {
 	// %c - STRING UNICODE
 	fmt.Printf("%c\n", 1400)
 	fmt.Printf("%c\n", 1235)
-	
+
 	// %d - STRING NUMERIK BASIS 10
 	fmt.Printf("%d\n", 1235)
 	fmt.Printf("%d\n", data.age)
-	
+
+	// %e %E - SCIENTIFIC NOTABLE
+	fmt.Printf("%e\n", data.height)
+
+	// %F - DECIMAL
+	fmt.Printf("%f\n", data.height)
+	// 182.500000
+	fmt.Printf("%.9f\n", data.height)
+	// 182.500000000
+	fmt.Printf("%.2f\n", data.height)
+	// 182.50
+	fmt.Printf("%.f\n", data.height)
+	// 182
+
+	// %G - FREE FLOW DECIMAL
+	fmt.Printf("%g\n", 0.123123123123)
+	// 0.123123123123
+
+	// %O - OCTAL BASE 8
+	fmt.Printf("%o\n", data.age)
+	// 32
+
+	// %p - FORMAT POINTER
+	fmt.Printf("%p\n", &data.name)
 }
