94e8dc927172b6bcbd10717a6690938d6b1de486
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-10 20:59:52 +0700

    Chapter A.40 -  Random Unique Seed

---
+++ a39-random/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	fmt.Println(rand.Int())
+	fmt.Println(rand.Int())
+	fmt.Println(rand.Int())
+}
