a6a2a598c5be64d821f7e410e19a12573887562b
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-10 22:37:21 +0700

    Chapter A.39 - Random string generator

--- a39-random/main.go
+++ a39-random/main.go
@@ -6,11 +6,29 @@ import (
 	"time"
 )
 
+// String Random
+var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 func main() {
 
+	// Unique Random Seed
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	fmt.Println(rand.Int())
 	fmt.Println(rand.Int())
 	fmt.Println(rand.Int())
+
+	a := randomString(10)
+
+	fmt.Println(a)
+}
+
+func randomString(length int) string {
+	b := make([]rune, length)
+
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+
+	return string(b)
 }
