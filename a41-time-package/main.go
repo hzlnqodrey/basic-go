4aed70342e1b1b20f3bf9c031bcae2e77fda050d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-07-28 23:17:48 +0700

    Simple I/O Quiz program with timer and goroutine combination

--- a41-time-package/main.go
+++ a41-time-package/main.go
@@ -2,59 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
-func main(){
-
-	// time.sleep
-	fmt.Println("Hello!")
-	time.Sleep(4 * time.Second)
-	fmt.Println("After 4 seconds!")
-
-	// Scheduler
-	// for true {
-	// 	fmt.Println("start!")
-	// 	time.Sleep(1 * time.Second)
-	// }
-	
-	// New Timer - Channeling and Blocking
-	var timer = time.NewTimer(4 * time.Second)
-	fmt.Println("start!")
-	<-timer.C
-	fmt.Println("finish")
-	
-	// time.AfterFunc - Channeling and Flex-Blocking
-	var ch = make(chan bool)
-	
-	time.AfterFunc(4 * time.Second, func(){
-		fmt.Println("expired")
+// TIMER - Goroutine
+func timer(timeout int, ch chan<- bool) {
+	time.AfterFunc(time.Duration(timeout)*time.Second, func() {
 		ch <- true
 	})
-	fmt.Println("start!")
-	<- ch
-	fmt.Println("finish")
+}
 
-	// time.After - Channel
-	<- time.After(2 * time.Second)
-	fmt.Println("finish")
+// WATCHER - Goroutine
+func watcher(timeout int, ch <-chan bool) {
+	<-ch
+	fmt.Println("\ntime out! no answer more than", timeout, "seconds")
+	os.Exit(0)
+}
 
-	// Scheduler with TICKER
-	done := make(chan bool)
-	ticker := time.NewTicker(time.Second)
+func main() {
+	var timeout = 5
+	var ch = make(chan bool)
 
-	go func() {
-		time.Sleep(10 * time.Second) // wait 5 sec
-		done <- true
-	}()
+	go timer(timeout, ch)
+	go watcher(timeout, ch)
 
-	for {
-		select {
-			case <- done:
-				ticker.Stop() // stopping the scheduler
-				return
-			case t := <- ticker.C:
-				fmt.Println("Current time: ", t)
-		}
+	var input string
+	fmt.Print("what is 625/25? ")
+	fmt.Scan(&input)
+
+	if input == "25" {
+		fmt.Println("the answer is correct!")
+	} else {
+		fmt.Println("the answer is wrong!")
 	}
 }
