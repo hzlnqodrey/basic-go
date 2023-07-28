6b5e90237c9c4860e730ee314e8103b2cb81de4b
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-07-28 22:31:08 +0700

    time.NewTimer() for channeling

--- a41-time-package/main.go
+++ a41-time-package/main.go
@@ -13,8 +13,14 @@ func main(){
 	fmt.Println("After 4 seconds!")
 
 	// Scheduler
-	for true {
-		fmt.Println("start!")
-		time.Sleep(1 * time.Second)
-	}
+	// for true {
+	// 	fmt.Println("start!")
+	// 	time.Sleep(1 * time.Second)
+	// }
+	
+	// New Timer - Channeling
+	var timer = time.NewTimer(4 * time.Second)
+	fmt.Println("start!")
+	<-timer.C
+	fmt.Println("finish")
 }
