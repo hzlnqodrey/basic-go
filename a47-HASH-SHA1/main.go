cf83e07dfe0ef5c90f21690c67b7c349a27a3b53
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-03 16:38:18 +0700

    hash sha1 algorithm using salt and time.Sleep so it can produce various encrypted SHA

--- a47-HASH-SHA1/main.go
+++ a47-HASH-SHA1/main.go
@@ -6,16 +6,38 @@ package main
 import(
 	"fmt"
 	"crypto/sha1"
+	"time"
 )
+
+func doHashUsingSalt(text string) (string, string) {
+	var salt = fmt.Sprintf("%d", time.Now().UnixNano())
+	var saltedText = fmt.Sprintf("text: %s, salt: %s", text, salt)
+	fmt.Println(saltedText)
+
+	var sha = sha1.New()
+	sha.Write([]byte(saltedText))
+	var encrypted = sha.Sum(nil)
+
+	time.Sleep(1 * time.Second)
+
+	return fmt.Sprintf("%x", encrypted), salt
+}
 
 func main() {
 	var text = "this is secret"
-	fmt.Println("Original data: ", text)
+	fmt.Printf("original : %s\n\n", text)
 	
-	var sha = sha1.New()
-	sha.Write([]byte(text))
-	var encrypted = sha.Sum(nil)
+	// hash 1
+	var hashed1, salt1 = doHashUsingSalt(text)
+	fmt.Printf("hashed - 1 : %s\n\n", hashed1)
 
-	var encryptedString = fmt.Sprintf("%x", encrypted)
-	fmt.Println("hashed: ", encryptedString)
+	// hash 2
+	var hashed2, salt2 = doHashUsingSalt(text)
+	fmt.Printf("hashed - 2 : %s\n\n", hashed2)
+	
+	// hash 3
+	var hashed3, salt3 = doHashUsingSalt(text)
+	fmt.Printf("hashed - 3 : %s\n\n", hashed3)
+	
+	_, _, _ = salt1, salt2, salt3
 }
