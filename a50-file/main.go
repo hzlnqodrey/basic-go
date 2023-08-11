e40f4e3170f7a8acd5a57b026a3fe5e7beb987b1
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-12 00:49:24 +0700

    edit file with os.WriteString() and save it with os.Sync()

--- a50-file/main.go
+++ a50-file/main.go
@@ -15,20 +15,42 @@ func isError(err error) bool {
 	return (err != nil)
 }
 
-func createFile() {
-	// deteksi apakah file sudah ada
-	var _, err = os.Stat(path)
+// func createFile() {
+// 	// deteksi apakah file sudah ada
+// 	var _, err = os.Stat(path)
 
-	// buat file baru jika belum ada
-	if os.IsNotExist(err) {
-		var file, err = os.Create(path)
-		if isError(err) { return }
-		defer file.Close()
-	}
+// 	// buat file baru jika belum ada
+// 	if os.IsNotExist(err) {
+// 		var file, err = os.Create(path)
+// 		if isError(err) { return }
+// 		defer file.Close()
+// 	}
 
-	fmt.Println("===> file berhasil dibuat", path)
+// 	fmt.Println("===> file berhasil dibuat", path)
+// }
+
+// edit file = buka akses write
+func writeFile() {
+	// buka file dengan level akses READ & WRITE
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) { return }
+	defer file.Close()
+
+	// tulis data ke file
+	_, err = file.WriteString("hello world\n")
+	if isError(err) { return }
+	
+	_, err = file.WriteString("Bab A50 - File Golang Basic Tutorial\n")
+	if isError(err) { return }
+
+	// save changes
+	err = file.Sync()
+	if isError(err) { return }
+
+	fmt.Println("===> file berhasil ditulis")
 }
 
 func main() {
-	createFile()
+	// createFile()
+	writeFile()
 }
