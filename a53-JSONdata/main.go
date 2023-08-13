b9439d7f750cc1e785f5900f4037d698668e563e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-13 19:04:59 +0700

    Decode Array JSON Ke Array Objek

--- a53-JSONdata/main.go
+++ a53-JSONdata/main.go
@@ -25,9 +25,29 @@ func main() {
 	fmt.Println("user :", data.FullName)
 	fmt.Println("age :", data.Age)
 
+	// === 53.2. Decode JSON Ke map[string]interface{}  &  interface{}
+
 	var data1 map[string]interface{}
 	json.Unmarshal(jsonData, &data1)
 
 	fmt.Println("user :", data1["Name"])
 	fmt.Println("age: ", data1["Age"])
+
+	// === 53.3 Decode Array JSON Ke Array Objek
+
+	var jsonStringArray = `[
+		{"Name": "Hazlan M Qodri", "Age": 23},
+		{"Name": "Dumby Dumb", "Age": 32}
+	]`
+
+	var dataUser []User
+
+	var err1 = json.Unmarshal([]byte(jsonStringArray), &dataUser)
+	if err1 != nil {
+		fmt.Println(err1.Error())
+		return
+	}
+
+	fmt.Println("User 1 :", dataUser[0].FullName, " - ", dataUser[0].Age)
+	fmt.Println("User 2 :", dataUser[1].FullName, " - ", dataUser[1].Age)
 }
