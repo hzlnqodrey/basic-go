b6f8ef212af9767a5f38087a2bba4cafd98ca6d9
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-13 18:57:30 +0700

    decode json string with json.Unmarshal() - equivalent of json.parse in javascript maybe

---
+++ a53-JSONdata/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "encoding/json"
+
+type User struct {
+	FullName string `json:"Name"`
+	Age int
+}
+
+func main() {
+	var jsonString = `{"Name": "Hazlan M Qodri", "Age": 23}`
+	var jsonData = []byte(jsonString)
+
+	var data User
+
+	// Fungsi unmarshal hanya menerima data json dalam bentuk  []byte
+	// Casting string -> byte
+	var err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("user :", data.FullName)
+	fmt.Println("age :", data.Age)
+	
+}
