9b54d5d4518a3361190cae16e10e93276c137bf5
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-21 10:34:12 +0700

    A.55 - HTTP Request with form data

--- a55-HTTP-Request-Client/main.go
+++ a55-HTTP-Request-Client/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"bytes"
+	"net/url"
 )
 
 var baseURL = "http://localhost:5000"
@@ -37,17 +39,56 @@ func fetchUsers() ([]student, error) {
 	}
 
 	return data, nil
+}
 
+func fetchUser(ID string) (student, error) {
+	var err error
+	var client = &http.Client{}
+	var data student
+
+	// process ID
+	var param = url.Values{}
+	param.Set("id", ID)
+	var payload = bytes.NewBufferString(param.Encode())
+
+	request, err := http.NewRequest("GET", baseURL+"/user", payload)
+	if err != nil {
+		return data, err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return data, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
 }
 
 func main() {
-	var users, err = fetchUsers()
+	// USERS
+	// var users, err = fetchUsers()
+	// if err != nil {
+	// 	fmt.Println("Error!", err.Error())
+	// 	return
+	// }
+
+	// for _, each := range users {
+	// 	fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.ID, each.Name, each.Grade)
+	// }
+
+	// USER
+	var user1, err = fetchUser("H001")
 	if err != nil {
 		fmt.Println("Error!", err.Error())
 		return
 	}
 
-	for _, each := range users {
-		fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.ID, each.Name, each.Grade)
-	}
+	fmt.Printf("ID: %s\t Name: %s\t Grade: %d\t", user1.ID, user1.Name, user1.Grade)
 }
